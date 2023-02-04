6663cafa2270afaf1c4027f3afa7d2ff56da3da7
silnith <silnith@gmail.com>
2023-02-04 03:15:01 -0600

    Ban Lombok.
    Even the authors of Lombok admit it is controversial.  They willfully
    violate the restrictions on how annotation processors are designed to be
    used, and code using Lombok is no longer valid Java.  It cannot be
    compiled without the annotation processor, so marking the plugin as
    "optional" is incorrect and misleading.
    
    Lombok is designed to hide boilerplate code behind opaque code
    generation.  But one of the primary reasons that Java has been so
    successful in the enterprise is precisely that it is explicit about what
    it is doing.  Hiding generated code is antithetical to this.  A
    developer should be able to easily confirm precisely how the final code
    behaves, whether the equals method allows inherited classes or not, what
    the format of the generated toString will be, etc.
    
    Everything that Lombok provides is already provided by all modern IDEs.
    Getters, setters, equals, hashcode, toString, and constructors are all
    generated by Eclipse, IntelliJ, NetBeans, etc., with only a few
    keystrokes and in seconds.  The IDE also provides clear outlines showing
    implemented methods and signatures as collapsible outlines, eliminating
    any problems with being able to quickly understand the structure and
    details of a class.  My experience has been that any claimed time saved
    by using Lombok, both in creating the generated code and in the
    cognitive load of viewing and understanding data objects, is vastly
    outweighed by the time lost debugging obscure build synchronization
    issues from the willful misuse of annotation processors.

--- src/main/java/com/midwesttape/project/challengeapplication/model/User.java
+++ src/main/java/com/midwesttape/project/challengeapplication/model/User.java
@@ -1,14 +1,80 @@
 package com.midwesttape.project.challengeapplication.model;
 
-import lombok.Data;
-import lombok.experimental.Accessors;
+import java.util.Objects;
 
-@Data
-@Accessors(chain = true)
 public class User {
     private Long id;
     private String firstName;
     private String lastName;
     private String username;
     private String password;
+
+    public User() {
+        super();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(final Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(final String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(final String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(final String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(final String password) {
+        this.password = password;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, id, lastName, password, username);
+    }
+
+    @Override
+    public boolean equals(final Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (!(obj instanceof User)) {
+            return false;
+        }
+        final User other = (User) obj;
+        return Objects.equals(firstName, other.firstName) && Objects.equals(id, other.id)
+                && Objects.equals(lastName, other.lastName) && Objects.equals(password, other.password)
+                && Objects.equals(username, other.username);
+    }
+
+    @Override
+    public String toString() {
+        return "User [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", username=" + username
+                + ", password=" + password + "]";
+    }
 }
