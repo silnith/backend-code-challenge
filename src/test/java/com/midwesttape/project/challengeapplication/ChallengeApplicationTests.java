4c4ebd34431f8990160ea6153aca080d302f6915
silnith <silnith@gmail.com>
2023-02-04 05:09:38 -0600

    Test that the user controller is injected.

--- src/test/java/com/midwesttape/project/challengeapplication/ChallengeApplicationTests.java
+++ src/test/java/com/midwesttape/project/challengeapplication/ChallengeApplicationTests.java
@@ -1,13 +1,26 @@
 package com.midwesttape.project.challengeapplication;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.midwesttape.project.challengeapplication.rest.UserController;
 
 @SpringBootTest
 class ChallengeApplicationTests {
+    
+    @Autowired
+    UserController userController;
 
     @Test
     void contextLoads() {
+    }
+    
+    @Test
+    void userController() {
+        assertNotNull(userController);
     }
 
 }
